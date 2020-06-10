356a38713c66f1700cbfb0cab7802a14b8ae0459
Nguyen Luong <kluong@cs.uchicago.edu>
2020-06-10 15:26:22 -0500

    final_data.pkl is now ready to input into run_model_selection

--- data_wrangling.py
+++ data_wrangling.py
@@ -7,8 +7,11 @@ tracts_filepath = 'data_sources/shape_tracts/tl_2018_17_tract.shp'
 places_filepath = 'data_sources/shape_places/tl_2018_17_place.shp'
 jobs_filepath = 'data_sources/il_jobs_by_tract_2017.csv'
 
+with open('CENSUS_DATA_COLS.json') as f:
+    DATA_COLS = json.load(f)
 
-def get_transitscore_data(lat, lng, city):
+
+def get_transitscore_data(lat, lng, city, api_key_index):
     '''
     Gets data for one location.
     
@@ -18,26 +21,19 @@ def get_transitscore_data(lat, lng, city):
         (json)
     '''
     rv = None
-    
-    #key = '4c4eb18a1eea25128110eaf683aefab4' #uchicago
-    #key = 'ffd1c56f9abcf84872116b4cc2dfcf31' #gmail
-    #key = 'b72221d8763203418d081f140357696e' #yahoo
-    #key = 'a311336c201153d1963484f73744a740' #aol
-    #key = '37b740e057b5b21f25e0d013026d66b4' #truman
-    #key = 'b8e1215244ca3c87e2018dfa731744b5' #outlook
-    key = 'b3cca8bcce0f65cd3fe82fb66f7a63c9' #protonmail
+
+    api_keys = ['4c4eb18a1eea25128110eaf683aefab4', '37b740e057b5b21f25e0d013026d66b4']
+    key = api_keys[api_key_index]
     
     url = 'https://transit.walkscore.com/transit/score/?lat={}&lon={}&city={}&state=IL&wsapikey={}'.format(lat, lng, city, key)
     r = requests.get(url)
     if not r.status_code==400:
         rv = r.json()
-    print(r)
-    print(r.text)
     
     return rv
 
 
-def create_transitscore_datalist(df):
+def create_transitscore_datalist(df, api_key_index):
     '''
     Turns list of jsons into a dataframe
     
@@ -49,15 +45,14 @@ def create_transitscore_datalist(df):
     datalist = []
     
     for row in df.itertuples():
-        print(row.centroid_lat, row.centroid_lng, row.place_name)
-        json = get_transitscore_data(row.centroid_lat, row.centroid_lng, row.place_name)
+        json = get_transitscore_data(row.centroid_lat, row.centroid_lng, row.place_name, api_key_index)
         if not json is None:
             json['tract_GEO_ID'] = row.tract_GEO_ID
             json['centroid_lat'] = row.centroid_lat
             json['centroid_lng'] = row.centroid_lng
             datalist.append(json)
 
-    return datalist
+    return transitscore_datalist
 
 
 def create_transitscore_dataframe(transitscore_datalist):
@@ -121,6 +116,7 @@ def place_data(places_filepath):
     all_places_data = places[['GEOID', 'NAME', 'NAMELSAD', 'geometry']] \
                             .rename(columns={'GEOID': 'place_GEO_ID', 'NAME': 'place_name',
                            'NAMELSAD': 'place_name_and_type'})
+                            
     return all_places_data
 
 
@@ -136,13 +132,18 @@ def add_transitscore(all_tracts_data, all_places_data):
         (pandas DataFrame)
     '''
     #Merging tracts and places
-    tracts_places = gpd.sjoin(tracts, places, how="inner", op="intersects")
+    tracts_places = gpd.sjoin(all_tracts_data, all_places_data, how="inner", op="intersects")
 
-    #Getting transit score data (may need to split tracts_place because of quota)
-    datalist = create_transitscore_datalist(tracts_places)
-    transitscore_by_tract = create_transitscore_dataframe(datalist)
+    #Splitting all tracts data into two dataframes because of api call limit
+    tp1 = tracts_places.iloc[:4750, :]
+    tp2 = tracts_places.iloc[4750:, :]
+    datalist1 = create_transitscore_datalist(tp1, 0)
+    datalist2 = create_transitscore_datalist(tp2, 1)
+    full_datalist = datalist1 + datalist2
+    transitscore_by_tract = create_transitscore_dataframe(full_datalist)
 
-    df = tracts.merge(transitscore_by_tract, on='tract_GEO_ID')
+    df = all_tracts_data.merge(transitscore_by_tract, on='tract_GEO_ID')
+
     return df 
 
 
@@ -151,16 +152,29 @@ def data_cleaning(df):
     Given full dataframe, calculates job density and population density,
     drops rows with zero population, and changes NaN values to zero.
     '''
+    #Calculating additional variables
     df['job_density'] = df['num_jobs'] / ((df['tract_area'])/1000000)
     df['pop_density'] = df['race_total'] / ((df['tract_area'])/1000000)
 
+    #Drop rows with zero population
     index_names = df[df['race_total']==0].index
     df.drop(index_names , inplace=True)
 
+    #Fill null value with zero
     df = df.fillna(0)
+
+    #Get numeric data
     df = df._get_numeric_data()
+
+    #Turn negative values of median income into null values 
     df[df < 0] = np.nan
-    df = df.drop(['year', 'centroid_lng', 'centroid_lat'], axis=1)
+
+    #Drop added columns used for calculating features
+    df = df.drop(['year', 'centroid_lng', 'centroid_lat', 'tract_area', 'num_jobs'], axis=1)
+
+    #Drop variables from ACS used for calculting features
+    keys = [key for key in list(DATA_COLS.values()) if key != 'GEO_ID']
+    df = df.drop(keys, axis=1)
 
     return df
 
@@ -181,9 +195,12 @@ def go(acs5, tracts_filepath, places_filepath, jobs_filepath, pickle_filename=No
     all_place_data = place_data(places_filepath)
     transit_score_added = add_transitscore(all_tract_data, all_place_data)
 
-    #Merging all data with ACS and writing to pickle file
+    #Merging all data with ACS and optionally write to pickle file
     acs5['tract_GEO_ID'] = acs5['GEO_ID'].apply(lambda x: x[9:])  
     full_df = pd.merge(acs5, transit_score_added, on='tract_GEO_ID')
     final_df = data_cleaning(full_df)
-    #final_df.to_pickle(filename)
+
+    if pickle_filename:
+        final_df.to_pickle(pickle_filename)
+
     return final_df
